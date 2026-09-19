1d64c37353e9eb4ff9ee3260a2381a50ab7f1770
Aaron Auld <88762795+aaronauld@users.noreply.github.com>
2026-09-19 22:45:20 +1000

    Add: non-overlapping-intervals - submission-1

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        # Problem: we want to remove the minimum number of intervals to have a non overlapping sequence
+        # Scarce/Demand: timeline is what is limited / intervals that need a spot
+        # Rule: sort by end time taking the interval that consumes the least on the timeline
+        
+        intervals = sorted(intervals, key = lambda x:x[1])
+        removed = 0
+        node = intervals[0]
+        for interval in intervals[1:]:
+            if node[1] > interval[0]:
+                removed += 1
+            else:
+                node = interval
+        return removed
